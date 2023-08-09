e989f942eca1c5b37e9d13b41c34d8386e9b2bbc
OadeO6 <abdurrahman.adenowo06@gmail.com>
2023-08-09 19:50:33 -0400

    made some corrections

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    for i in range(str):
+    new = ""
+    for i in range(len(str)):
         if i == n:
-            continue;
+            continue
         new += str[i]
     return new
+
+
 print(remove_char_at("ADENOWO", 2))
 print(remove_char_at("ADENOWO", -2))
