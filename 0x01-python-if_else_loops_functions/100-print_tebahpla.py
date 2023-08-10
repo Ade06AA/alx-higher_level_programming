38c9ced1f4b34b9b3e37d4c48fdf2c8cce1e6077
OadeO6 <abdurrahman.adenowo06@gmail.com>
2023-08-09 20:22:48 -0400

    made some corrections

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for ch range (122,98,-1):
-    print("{:c}".format(ch if ch % 2 == 0 else ch - 32), end = "")
+for ch in range(122, 96, -1):
+    print ("{:c}".format(ch if ch % 2 == 0 else ch - 32), end="")
