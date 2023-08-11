c54ee582e0538932e74b3084d21a12f1d6d3c875
OadeO6 <abdurrahman.adenowo06@gmail.com>
2023-08-11 19:35:25 -0400

    first commit

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 
-import calculator_1 as mycal
-
 if __name__ == "__main__":
+    import calculator_1 as mycal
     a = 10
     b = 5
     print(f"{a:d} + {b:d} = {mycal.add(a, b):d}")
