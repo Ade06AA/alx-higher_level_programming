553cb5521fa89fb15d6693ee32b9e8215911fa48
OadeO6 <abdurrahman.adenowo06@gmail.com>
2023-08-12 13:40:13 -0400

    first commit

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,9 +2,10 @@
 
 if __name__ == "__main__":
     from sys import argv
-    if len(argv) == 1:
+    argc = len(argv)
+    if argc == 1:
         print("0 arguments.")
     else:
-        print("{} argaument:".format(len(argv) - 1))
-        for i in range(len(argv) - 1):
-            print(f"{i + 1:d}: {argv[i + 1]:s}")
+        print("{} argument{}:".format(argc - 1, 's' if argc > 2 else ''))
+        for i in range(argc - 1):
+            print("{:d}: {:s}".format(i + 1, argv[i + 1]))
