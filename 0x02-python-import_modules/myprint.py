7fd9446290a16c20ad5219d72c067a776610492e
OadeO6 <abdurrahman.adenowo06@gmail.com>
2023-08-12 14:53:05 -0400

    made some corection

--- 0x02-python-import_modules/myprint.py
+++ 0x02-python-import_modules/myprint.py
@@ -1,2 +1,6 @@
 import os
-os.write(1, b'#pythoniscool')
+os.write(1, b'#pythoniscool\n')
+# the above iis the same as writing
+# __import__('os').write(1, '#pythoniscool\n'.encode())
+# or
+# __import__('os').write(1, b'#pythoniscool\n')
