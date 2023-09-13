2d03a36cdfc796abfb0aaf2f4f11dd789909b559
OadeO6 <abdurrahman.adenowo06@gmail.com>
2023-09-13 11:02:18 -0400

    update

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -9,6 +9,5 @@ def save_to_json_file(my_obj, filename):
     """
     larning how to use json in python
     """
-    js = myjson.dumps(my_obj)
     with open(filename, 'w', encoding='utf-8') as jf:
-        jf.write(js)
+        jf.write(myjson.dumps(my_obj))
