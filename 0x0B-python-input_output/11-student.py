ceda42db2b7947ba858c8bedce128f1a774cdfdf
OadeO6 <abdurrahman.adenowo06@gmail.com>
2023-09-13 15:21:20 -0400

    update

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+module doc
+"""
+
+
+class Student:
+    """
+    class doc
+    """
+
+    def __init__(self, fn, ln, age):
+        """
+        func doc
+        """
+        self.first_name = fn
+        self.last_name = ln
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        func doc
+        """
+        tt = self.__dict__
+        if attrs is None:
+            return tt
+        else:
+            t = {}
+            for i in attrs:
+                if i in tt.keys():
+                    t[i] = tt[i]
+            return t
+    def reload_from_json(self, json):
+        """
+        func def
+        """
+        for g in json.keys():
+            self.g = json[g]
